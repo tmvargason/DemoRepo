83b1ce8a99cea2aae89a03266bdf1bd25c74502e
Tesia Vargason <ttt@gmail.com>
2021-01-10 12:30:07 -0500

    second try for search page features

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -2,4 +2,9 @@ package com.qa.pages;
 
 public class HomePage {
 
+	public void homePage() {
+		System.out.println("home page");
+	}
+	
+	
 }
--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,5 +1,12 @@
 package com.qa.tests;
 
+import org.junit.Test;
+
 public class LoginTest {
+	
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
 
 }
